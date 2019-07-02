f550c13f32e0512c91ddbe4fa604bc618b7fb813
Matthew Dague <mattdague@gmail.com>
2019-07-02 10:03:08 -0400

    added equation for generating target number and wrote comments psuedo coding how to proceed

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+var userNum = 0
+var targetNum = Math.floor(Math.random() * 102 + 19)
+console.log(targetNum);
+
+
+//write function crystalNum
+//return write formula for crystal num
+// in start function set var = crystalNum()
