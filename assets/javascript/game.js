f3a57ad7e7b0b8360e09187820157849b3e6718d
Matthew Dague <mattdague@gmail.com>
2019-07-02 17:27:46 -0400

    most of game finished, alls buttons are coded and combine to user total, win/loss still not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,118 @@
 
-//function for determining new game
-function gameStart () {
-    var userNum = 0
+winTotal = 0
+lossTotal = 0
+
+
+
+//function starting new game
+function gameStart() {
+    //sets user number to 0 and updates html to reflect that
+    var userNum = 0;
+    $("#score").html(userNum);
+
     var redNum = crystalNum();
     var blueNum = crystalNum();
     var yellowNum = crystalNum();
     var greenNum = crystalNum();
     var targetNum = target();
 
+    $("#goal").html(targetNum);
+
     console.log(redNum);
     console.log(blueNum);
     console.log(yellowNum);
     console.log(greenNum);
     console.log(targetNum);
-}
 
-//function for determining the goal number
-function target () {
-    return Math.floor(Math.random() * 102 + 19)
-}
+    $("#red").on("click", addRed);
+    $("#blue").on("click", addBlue);
+    $("#yellow").on("click", addYellow);
+    $("#green").on("click", addGreen);
 
 
-//function for determining random values of each crystal
-function crystalNum() {
-    return Math.floor(Math.random() * 12 + 1);
+
+
+
+    //function for determining the goal number
+    function target() {
+        return Math.floor(Math.random() * 102 + 19)
+    }
+
+    //function for determining random values of each crystal
+    function crystalNum() {
+        return Math.floor(Math.random() * 12 + 1);
+    }
+
+    function addRed() {
+        userNum = (userNum + redNum);
+        $("#score").html(userNum);
+        console.log(userNum);
+        console.log(targetNum);
+    }
+
+    function addBlue() {
+        userNum = (userNum + blueNum);
+        $("#score").html(userNum);
+        console.log(userNum);
+        console.log(targetNum);
+    }
+
+    function addYellow() {
+        userNum = (userNum + yellowNum);
+        $("#score").html(userNum);
+        console.log(userNum);
+        console.log(targetNum);
+    }
+
+    function addGreen() {
+        userNum = (userNum + greenNum);
+        $("#score").html(userNum);
+        console.log(userNum);
+        console.log(targetNum);
+        
+    }
+
+    if (userNum === targetNum) {
+        winGame()
+    }
+
+    else if (userNum > targetNum) {
+        loseGame()
+    }
+
+//function to add to win column and restart game
+    function winGame() {
+        winTotal++;
+        $("#wins").html("Wins: " + winTotal);
+        gameStart()
+       
+    }
+// function to add to loss column and restart game
+    function loseGame() {
+        lossTotal++;
+        $("#losses").html("Losses: " + lossTotal);
+        gameStart();
+    }
+
+
+   
 }
 
 gameStart();
+
+
+// function winGame(){
+//  winTotal++;
+//  $("#wins").html("Wins: " + winTotal);
+// gameStart() 
+// // return winTotal;
+// }
+
+// function loseGame() {
+//     lossTotal++;
+//     $("#losses").html("Losses: " + lossTotal);
+//     // return lossTotal;
+//     gameStart();
+// }
+
+
