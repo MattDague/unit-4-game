1ba66b1df9597c7bc928dadf3f459c7546ee9d88
Matthew Dague <mattdague@gmail.com>
2019-07-02 16:20:36 -0400

    added functions and variables for determining crystals values at start, created gamestart conditions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,29 @@
-var userNum = 0
-var targetNum = Math.floor(Math.random() * 102 + 19)
-console.log(targetNum);
+
+//function for determining new game
+function gameStart () {
+    var userNum = 0
+    var redNum = crystalNum();
+    var blueNum = crystalNum();
+    var yellowNum = crystalNum();
+    var greenNum = crystalNum();
+    var targetNum = target();
+
+    console.log(redNum);
+    console.log(blueNum);
+    console.log(yellowNum);
+    console.log(greenNum);
+    console.log(targetNum);
+}
+
+//function for determining the goal number
+function target () {
+    return Math.floor(Math.random() * 102 + 19)
+}
 
 
-//write function crystalNum
-//return write formula for crystal num
-// in start function set var = crystalNum()
+//function for determining random values of each crystal
+function crystalNum() {
+    return Math.floor(Math.random() * 12 + 1);
+}
+
+gameStart();
