51e00af310ae8105f4fd8c4246c838e258c85c13
Matthew Dague <mattdague@gmail.com>
2019-07-03 14:45:52 -0400

    game working and finished, moved variables and functions outside of main function, needs some commenting and cleanup still though

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,19 +2,33 @@
 winTotal = 0
 lossTotal = 0
 
+var userNum
+$("#score").html(userNum);
+
+var redNum
+var blueNum
+var yellowNum
+var greenNum
+var targetNum = target();
+
+$("#goal").html(targetNum);
+$("#red").on("click", addRed);
+$("#blue").on("click", addBlue);
+$("#yellow").on("click", addYellow);
+$("#green").on("click", addGreen);
 
 
 //function starting new game
 function gameStart() {
     //sets user number to 0 and updates html to reflect that
-    var userNum = 0;
+    userNum = 0;
     $("#score").html(userNum);
 
-    var redNum = crystalNum();
-    var blueNum = crystalNum();
-    var yellowNum = crystalNum();
-    var greenNum = crystalNum();
-    var targetNum = target();
+    redNum = crystalNum();
+    blueNum = crystalNum();
+    yellowNum = crystalNum();
+    greenNum = crystalNum();
+    targetNum = target();
 
     $("#goal").html(targetNum);
 
@@ -24,84 +38,70 @@ function gameStart() {
     console.log(greenNum);
     console.log(targetNum);
 
-    $("#red").on("click", addRed);
-    $("#blue").on("click", addBlue);
-    $("#yellow").on("click", addYellow);
-    $("#green").on("click", addGreen);
-
-
-
-
 
     //function for determining the goal number
-    function target() {
-        return Math.floor(Math.random() * 102 + 19)
-    }
-
-    //function for determining random values of each crystal
-    function crystalNum() {
-        return Math.floor(Math.random() * 12 + 1);
-    }
-
-    function addRed() {
-        userNum = (userNum + redNum);
-        $("#score").html(userNum);
-        testWin();
-        console.log(userNum);
-        console.log(targetNum);
-    }
-
-    function addBlue() {
-        userNum = (userNum + blueNum);
-        $("#score").html(userNum);
-        testWin();
-        console.log(userNum);
-        console.log(targetNum);
-    }
-
-    function addYellow() {
-        userNum = (userNum + yellowNum);
-        $("#score").html(userNum);
-        testWin();
-        console.log(userNum);
-        console.log(targetNum);
-    }
-
-    function addGreen() {
-        userNum = (userNum + greenNum);
-        $("#score").html(userNum);
-        testWin();
-        console.log(userNum);
-        console.log(targetNum);
-
-    }
-
-    //function to add to win column and restart game
-
-    function testWin() {
-        if (userNum === targetNum) {
-            winGame()
-        }
-
-        else if (userNum > targetNum) {
-            loseGame()
-        }
-    }
 
 }
 
-gameStart();
 
 
-// function testWin () {
-// if (userNum === targetNum) {
-//     winGame()
-// }
 
-// else if (userNum > targetNum) {
-//     loseGame()
-// }
-// }
+function target() {
+    return Math.floor(Math.random() * 102 + 19)
+}
+
+
+//function for determining random values of each crystal
+function crystalNum() {
+    return Math.floor(Math.random() * 12 + 1);
+}
+
+function addRed() {
+    userNum = (userNum + redNum);
+    $("#score").html(userNum);
+    testWin();
+    console.log(userNum);
+    console.log(targetNum);
+}
+
+function addBlue() {
+    userNum = (userNum + blueNum);
+    $("#score").html(userNum);
+    testWin();
+    console.log(userNum);
+    console.log(targetNum);
+}
+
+function addYellow() {
+    userNum = (userNum + yellowNum);
+    $("#score").html(userNum);
+    testWin();
+    console.log(userNum);
+    console.log(targetNum);
+}
+
+function addGreen() {
+    userNum = (userNum + greenNum);
+    $("#score").html(userNum);
+    testWin();
+    console.log("usernum" + userNum);
+    console.log("targetnum" + targetNum);
+
+}
+
+
+
+//function to add to win column and restart game
+
+function testWin() {
+    if (userNum === targetNum) {
+        winGame()
+    }
+
+    else if (userNum > targetNum) {
+        loseGame()
+    }
+}
 
 function winGame() {
     winTotal++;
@@ -116,3 +116,4 @@ function loseGame() {
     gameStart();
 }
 
+gameStart();
